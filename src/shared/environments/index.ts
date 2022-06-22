899a5bc0b445bee6bd06a0aa4c3e91e1c4675fe8
felipe <fl.louzeiro@gmail.com>
2022-06-22 12:21:50 -0300

    :sparkles: feat: implementando service de pessoas

--- src/shared/environments/index.ts
+++ src/shared/environments/index.ts
@@ -1,6 +1,21 @@
 export const Environment = {
   /**
+   * Define a quantidade linhas a ser carregada nas listagens.
+   */
+  LIMITE_DE_LINHAS: 10,
+
+  /**
    * Placeholder exibido nas inputs.
    */
   INPUT_DE_BUSCA: 'Busque livros pelo título, autor ou idioma',
+
+  /**
+   * Texto exibido quando nenhum livro é encontrado em um listagem.
+   */
+  LISTAGEM_VAZIA: 'Nenhum livro encontrado.',
+
+  /**
+   * Url base de consulta dos dados dessa aplicação.
+   */
+  URL_BASE: 'http://localhost:4000',
 };
---
+++ src/shared/services/api/LivrosService/index.ts
@@ -0,0 +1,48 @@
+import { Environment } from '../../../environments';
+import { Api } from '../axios-config';
+
+export interface IListagemLivros {
+  author: string;
+  country: string;
+  imageLink: string;
+  language: string;
+  link: string;
+  pages: number;
+  title: string;
+  year: number;
+}
+
+type ILivrosComTotalCount = {
+  data: IListagemLivros[];
+  totalCount: number;
+};
+
+const getAll = async (
+  page = 1,
+  filter = ''
+): Promise<ILivrosComTotalCount | Error> => {
+  try {
+    const urlRelativa = `/books?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&title_like=${filter}`;
+
+    const { data, headers } = await Api.get(urlRelativa);
+
+    if (data) {
+      return {
+        data,
+        totalCount: Number(
+          headers['x-total-count'] || Environment.LIMITE_DE_LINHAS
+        ),
+      };
+    }
+
+    return new Error('Erro ao listar os livros');
+  } catch (error) {
+    return new Error(
+      (error as { message: string }).message || 'Erro ao listar os livros'
+    );
+  }
+};
+
+export const LivrosService = {
+  getAll,
+};
